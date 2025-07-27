16285c42ae6430b1c310598bfc68eac050ec6fcc
bhumikakel <bhumikabscit@gmail.com>
2025-07-27 18:31:01 +0530

    Added seats in email-error solved

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -94,25 +94,26 @@ const sendBookingConfirmationEmail = inngest.createFunction(
     }).populate('user');
 
     await sendEmail({
-      to:booking.user.email,
-      subject:`Payment Confirmation: "${booking.show.movie.title}" booked!`,
-      body:`<div style="font-familt: Arial, sans-serif; line-height: 1.5;">
+  to: booking.user.email,
+  subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+  body: `
+    <div style="font-family: Arial, sans-serif; line-height: 1.5;">
       <h2>Hi ${booking.user.name},</h2>
       <p>Your booking for <strong style="color:#F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
       <p>
-      <strong>Date:</strong>${new Date(booking.show.showDateTime).toLocaleDateString('en-US',{timeZone:'Asia/kolkata'})}<strong>Date:</strong>${new Date(booking.show.showDateTime).toLocaleTimeString('en-US',{timeZone:'Asia/kolkata'})}
+        <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br>
+        <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
       </p>
-      <p>Your Booked Seats are: <strong style={{ color: "#9f29d5ff" }}>
-        {booking.bookedSeats.join(", ")}
-      </strong></p>
-      <p>Enjoy the show! </p>
-      <p>Thanks for booking with us! <br>-Moview Team</p>
-      </div>
-
-      `
-    })
-  }
-)
+      <p>Your Booked Seats are: 
+        <strong style="color: #9f29d5ff;">
+          ${booking.bookedSeats.join(", ")}
+        </strong>
+      </p>
+      <p>Enjoy the show!</p>
+      <p>Thanks for booking with us!<br>- Moview Team</p>
+    </div>
+  `
+});
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation,releaseSeatsAndDeleteBooking,sendBookingConfirmationEmail];
